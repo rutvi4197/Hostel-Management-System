09fc54dcc71a34adfd2da5c1f532d5a2ef5d634c
nisha-parikh <nishaparikh71@gmail.com>
2018-03-22 13:42:11 +0530

    login page added

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -1,5 +1,26 @@
-<link href="<?php echo base_url(); ?>css/login.css" rel="stylesheet" type="text/css" >
-	
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Login</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link  href="<?php echo base_url(); ?>bootstrapcss/bootstrap.min.css" rel="stylesheet">
+  <link  href="<?php echo base_url(); ?>bootstrapcss/font-awesome.min.css" rel="stylesheet">
+  <script src="<?php echo base_url(); ?>js/jquery.min.js"></script>
+  <script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
+  <link href="<?php echo base_url(); ?>css/login.css" rel="stylesheet" type="text/css" >
+</head>
+<body>
+
+<!--
+    you can substitue the span of reauth email for a input with the email and
+    include the remember me checkbox
+    -->
+<div class="row">
+    <div class="col-xs-3 col-sm-2 col-md-1"><img class="logo" style="width:auto;height:100px;" src="image/Logo.jpg"></div>
+    <div class="col-xs-9 col-sm-10 col-md-11"><img class="logoText" src="image/logoText.jpg"></div>
+</div>
+
 	<div class="container">
     <div class="row">
         <div class="login">
@@ -11,4 +32,6 @@
         </form>
         </div>
     </div>
-</div>
+	</div>
+</body>
+</html>
